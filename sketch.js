9ace464a2439b93271138bec45b409c9757e1ab2
rayc2045 <>
2022-10-28 15:33:24 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,10 @@
 function setup() {
   createCanvas(500, 500)
-  background(220)
+  // background(220)
 }
 
 function draw() {
-  fill(255)
+  background(mouseX, 207, 150 + frameCount / 5, 20)
+  fill(255, mouseX - 200, 255 - mouseY)
   ellipse(mouseX, mouseY, 80, 80)
 }
