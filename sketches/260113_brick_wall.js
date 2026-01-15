4c89c0144d64549f5b467cd3394550431e55ed14
Ray C <rayc2045@gmail.com>
2026-01-15 10:24:28 +0800

    Update the sketch "brick wall"

--- sketches/260113_brick_wall.js
+++ sketches/260113_brick_wall.js
@@ -1,26 +1,29 @@
+const [width, height] = [800, 800];
+
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(width, height);
   background("rosybrown");
 }
 
-let x = -10,
-  y = 10,
+let gap = 8,
   h = 20,
-  gap = 8;
+  x = -10,
+  y = height - gap - h;
 
 function draw() {
   let w = random(h * 1.5, h * 4);
 
-  if (frameCount % 5 === 0) fill("silver");
+  if (frameCount % 9 === 0) fill("silver");
   else if (w > h * 3) fill("darkred");
   else if (w > h * 2) fill("firebrick");
   else fill("crimson");
 
   rect(x, y, w, h);
-  x += w + gap;
 
-  if (x >= 800 - gap && y < 800 - h - gap * 2) {
+  if (x < width && y >= -gap - h) {
+    x += w + gap;
+  } else {
+    y -= h + gap;
     x = random(-gap, -gap * 2);
-    y += h + gap;
   }
 }
