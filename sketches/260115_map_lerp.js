5f986297e44dfeb28c3886d6cc9cbb2c4229508c
Ray C <rayc2045@gmail.com>
2026-01-15 20:58:22 +0800

    Add a sketch

---
+++ sketches/260115_map_lerp.js
@@ -0,0 +1,25 @@
+const { log } = console;
+const [width, height] = [800, 800];
+
+function setup() {
+  createCanvas(width, height);
+  background("gainsboro");
+}
+
+let x = 0,
+  y = 0,
+  d = 0;
+
+function draw() {
+  // x = random(width);
+  // y = random(height);
+  d = mouseX / 30;
+  x += random(-d, d);
+  y += random(-d, d);
+  x = lerp(x, mouseX, 0.1);
+  y = lerp(y, mouseY, 0.1);
+  const R = map(mouseX, 0, width, 0, 255, true);
+  // log(R);
+  fill(R, 0, 0);
+  circle(x, y, 50);
+}
