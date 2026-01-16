91ebb6f5ab28a3dbfaa462370d1f6ef6f0d2dce2
Ray C <rayc2045@gmail.com>
2026-01-16 17:18:39 +0800

    Add a sketch

---
+++ sketches/260116_loop.js
@@ -0,0 +1,21 @@
+const width = 800,
+  height = 800;
+
+function setup() {
+  createCanvas(width, height);
+  background("gainsboro");
+}
+
+function draw() {
+  background(0);
+  for (let i = 0; i < 50; i++) {
+    noFill();
+    strokeWeight(5);
+    stroke(map(i, 0, 10, 0, 255), 0, 255);
+    ellipse(
+      width / 2,
+      height / 2,
+      800 - i * 25 - i * map(mouseX, 0, width, 0, 20),
+    );
+  }
+}
