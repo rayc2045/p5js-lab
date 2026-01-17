6ee8a58b90d9c021613975826600431b51221771
Ray C <rayc2045@gmail.com>
2026-01-17 17:49:18 +0800

    Add utils.js

---
+++ utils.js
@@ -0,0 +1,9 @@
+function getSearchString(str) {
+  return new URLSearchParams(window.location.search).get(str);
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
