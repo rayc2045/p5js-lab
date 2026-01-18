74e3de12ef26db0061fb49c50ab5f9a5fa73f008
Ray C <rayc2045@gmail.com>
2026-01-18 17:24:55 +0800

    Add a brush

--- sketches/260117_noise_brush.js
+++ sketches/260117_noise_brush.js
@@ -7,17 +7,32 @@ function draw() {
   // background(0, 1);
 }
 
+let isNoise = true;
+
+function mousePressed() {
+  isNoise = !isNoise;
+}
+
 function mouseMoved() {
   let count = getRandomInt(100, 200),
+    delta = sqrt(dist(pmouseX, pmouseY, mouseX, mouseY)) * 5,
     r = random(2, 20);
   for (let i = 0; i < count; i++) {
     noStroke();
-    fill(
-      random((frameCount % 255) + mouseY),
-      random(200, 255),
-      random(100, 200),
-    );
-    ellipse(mouseX + random(-50, 50), mouseY + random(-50, 50), r);
-    r *= 0.9;
+    if (isNoise) {
+      fill(
+        random((frameCount % 255) + mouseY),
+        random(200, 255),
+        random(100, 200),
+      );
+      ellipse(
+        mouseX + random(-delta, delta),
+        mouseY + random(-delta, delta),
+        r,
+      );
+      r *= 0.9;
+    } else {
+      ellipse(mouseX, mouseY, 50, 50);
+    }
   }
 }
