65141d0fe10f7d6c329797e2d94e1211daf13646
Ray C <rayc2045@gmail.com>
2026-02-05 21:27:37 +0800

    Add a sketch

---
+++ sketches/260205_falling_flowers.js
@@ -0,0 +1,143 @@
+const s = Math.min(innerWidth, innerHeight) * 0.9,
+  width = s,
+  height = s;
+
+const sizes = ["XS", "S", "M", "L"],
+  stamenColors = [
+    "pink",
+    "lightyellow",
+    "lemonchiffon",
+    "papayawhip",
+    "moccasin",
+    "peachpuff",
+    "gold",
+    "palegoldenrod",
+    "khaki",
+    "cornsilk",
+    "blanchedalmond",
+    "beige",
+    "seashell",
+    "oldlace",
+    "floralwhite",
+    "ivory",
+    "antiquewhite",
+    "linen",
+    "lavenderblush",
+    "mistyrose",
+  ],
+  petalColors = [
+    "indianred",
+    "lightcoral",
+    "salmon",
+    "darksalmon",
+    "lightsalmon",
+    "lightpink",
+    "lightpink",
+    "hotpink",
+    "palevioletred",
+    "coral",
+    "tomato",
+    "orangered",
+    "darkorange",
+    "orange",
+    "gold",
+    "darkseagreen",
+    "mediumaquamarine",
+    "lightseagreen",
+    "paleturquoise",
+    "aquamarine",
+    "turquoise",
+    "mediumturquoise",
+    "darkturquoise",
+    "cadetblue",
+    "lightsteelblue",
+    "powderblue",
+    "lightblue",
+    "skyblue",
+    "lightskyblue",
+    "cornflowerblue",
+    "lavender",
+    "thistle",
+    "plum",
+    "violet",
+    "orchid",
+    "mediumorchid",
+    "mediumpurple",
+    "sandybrown",
+    "antiquewhite",
+    "mistyrose",
+  ],
+  flowers = [],
+  appendFlower = (x, y) => {
+    flowers.push({
+      x: x || random(width),
+      y: y || -120,
+      size: random(sizes),
+      angle: random(15),
+      a: random(0.002, 0.006) * random([1, -1]),
+      stamenColor: random(stamenColors),
+      petalColor: random(petalColors),
+      velocity: random(1, 3),
+    });
+  };
+
+function setup() {
+  createCanvas(width, height);
+  background("gainsboro");
+  appendFlower();
+}
+
+function draw() {
+  if (frameCount % 90 === 0) appendFlower();
+  background("gainsboro");
+
+  for (let i = 0; i < flowers.length; i++) {
+    push();
+
+    const { x, y, size, angle, a, stamenColor, petalColor, velocity } =
+      flowers[i];
+    translate(x, y);
+
+    let w, h;
+    if (size === "XS") {
+      w = 30;
+      h = 12;
+    } else if (size === "S") {
+      w = 50;
+      h = 20;
+    } else if (size === "M") {
+      w = 80;
+      h = 30;
+    } else {
+      w = 120;
+      h = 40;
+    }
+
+    rotate(angle);
+    flowers[i].angle += a;
+    fill(stamenColor);
+    // ellipseMode(CENTER);
+    ellipse(0, 0, w / 2.5);
+
+    ellipseMode(CORNER);
+    const numOfPetals = 16,
+      gap = w / 4;
+    for (let i = 0; i < numOfPetals; i++) {
+      fill(petalColor);
+      ellipse(gap, -h / 2, w, h);
+      line(gap, 0, w + gap, 0);
+      rotate(PI / (numOfPetals / 2));
+    }
+
+    flowers[i].y += velocity;
+    if (flowers[i].y > height + w + gap) flowers.splice(i, 1);
+
+    pop();
+  }
+
+  log(flowers.length);
+}
+
+function mouseClicked() {
+  appendFlower(mouseX, mouseY);
+}
